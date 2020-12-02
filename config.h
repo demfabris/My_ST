1fee01ca03561b0340da8321c75255a418e9cede
fabricio7p <fabriciodematte7p@gmail.com>
2020-12-02 01:23:33 -0300

    feat: onedark theme

--- config.h
+++ config.h
@@ -107,31 +107,22 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+  "#282c34",
+  "#e06c75",
+  "#98c379",
+  "#d19a66",
+  "#61afef",
+  "#c678dd",
+  "#56b6c2",
+  "#abb2bf",
+  "#5c6370",
+  "#e06c75",
+  "#98c379",
+  "#d19a66",
+  "#61afef",
+  "#c678dd",
+  "#56b6c2",
+  "#ffffff"
 };
 
 
@@ -141,8 +132,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 7;
+static unsigned int defaultrcs = 0;
 
 /*
  * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Ps-SP-q.1D81
