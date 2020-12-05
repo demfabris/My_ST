48a99f55a4753d55233537aec992418c811ccf2a
fabricio7p <fabriciodematte7p@gmail.com>
2020-12-05 01:04:15 -0300

    patch: font2

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "FantasqueSansMono Nerd Font:size=14:antialias=true:autohint=true";
+static char *font2[] = {"Fantasque Sans Mono Nerd Font:style=italic:size=14:antialias=true:autohint=true" };
 static int borderpx = 1;
 
 /*
