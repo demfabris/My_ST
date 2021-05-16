ed9d4d5068b68b8b9c8fa313577c4d3f14f65ab8
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-05-16 19:47:26 -0300

    remove padding

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "FantasqueSansMono Nerd Font:size=16:antialias=true:autohint=true";
 static char *font2[] = {"Noto", "NotoEmoji", "NotoColorEmoji"};
-static int borderpx = 2;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
